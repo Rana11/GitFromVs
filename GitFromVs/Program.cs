e4c99d287a6fb17783bd5c417e09397324731d8d
Ran Alkhatib <eng.rana11@gmail.com>
2016-03-31 15:40:03 +0100

    V 1.0

---
+++ GitFromVs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFromVs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // v 1.0
+        }
+    }
+}
