5102a36b47751df459d9a01db1af91c1114d2ce0
Ran Alkhatib <eng.rana11@gmail.com>
2016-03-31 15:56:50 +0100

    1.8

--- GitFromVs/MyClass.cs
+++ GitFromVs/MyClass.cs
@@ -2,6 +2,6 @@
 {
     class MyClass
     {
-        //v 1.4
+        //v 1.8
     }
 }
--- GitFromVs/Program.cs
+++ GitFromVs/Program.cs
@@ -10,7 +10,7 @@ namespace GitFromVs
     {
         static void Main(string[] args)
         {
-            // v 1.6
+            // v 1.8
         }
     }
 }
